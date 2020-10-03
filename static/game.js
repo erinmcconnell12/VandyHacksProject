d876cded65d60ba175fe54b3b0379c68db0a3346
millanisaac <9millanisaac9@gmail.com>
2020-10-03 12:29:32 -0700

    added player

--- static/game.js
+++ static/game.js
@@ -70,3 +70,18 @@ socket.on('state', function(players) {
     context.fill();
   }
 });
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players) {
+    context.clearRect(0, 0, 800, 600);
+    context.fillStyle = 'green';
+    for (var id in players) {
+        var player = players[id];
+        context.beginPath();
+        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+        context.fill();
+    }
+});
